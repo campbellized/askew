9cc2866e7e180ee5b8fc902dd6ebd60a20562ece
campbellized <campbellized@gmail.com>
2016-11-23 19:43:40 -0500

    Initial commit.

---
+++ askew.py
@@ -0,0 +1,17 @@
+"""Askew is an image viewer that queries Google Images and displays the results
+as ASCII art.
+
+Usage
+-----
+    $ python askew.py
+"""
+
+
+def main():
+    """This is a placeholder.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
