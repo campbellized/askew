11f7e9d7d3930acc8cf0706228008a9aec4dd077
campbellized <campbellized@gmail.com>
2016-11-23 20:50:49 -0500

    Askew can retrieve and parse HTML using requests and BeautifulSoup

--- askew.py
+++ askew.py
@@ -6,11 +6,38 @@ Usage
     $ python askew.py
 """
 
+from bs4 import BeautifulSoup
+
+import requests
+import re
+
 
 def main():
     """This is a placeholder.
     """
-    pass
+
+    while True:
+        query = input("What is your query?\n")
+
+        if str(query):
+            break
+        else:
+            print("Please enter a valid string")
+
+    query = filter_input(query)
+    url = "https://www.google.com/search?site=&tbm=isch&q=" + query
+    print(url)
+
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, "html.parser")
+
+
+def filter_input(query):
+    """Takes a string and prepares it to be used in a web query
+    """
+
+    return re.sub("\s+", "+", query)
 
 
 if __name__ == "__main__":
