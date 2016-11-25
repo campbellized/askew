64b6c901f048d2159dae8403230db91b68897952
campbellized <campbellized@gmail.com>
2016-11-25 10:03:12 -0500

    Askew can successfull retrieve images and save to TMP directory.

--- askew.py
+++ askew.py
@@ -1,4 +1,4 @@
-"""Askew is an image viewer that queries Google Images and displays the results
+"""Askew is an image viewer that searches for images and displays the results
 as ASCII art.
 
 Usage
@@ -11,26 +11,46 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+DATA_PATH = "tmp/"
+
 
 def main():
     """This is a placeholder.
     """
 
-    while True:
-        query = input("What is your query?\n")
-
-        if str(query):
-            break
-        else:
-            print("Please enter a valid string")
-
-    query = filter_input(query)
-    url = "https://www.google.com/search?site=&tbm=isch&q=" + query
+    # while True:
+    #     query = input("What is your query?\n")
+    #
+    #     if str(query):
+    #         break
+    #     else:
+    #         print("Please enter a valid string")
+    #
+    # query = filter_input(query)
+    # url = "http://www.deviantart.com/browse/all/?section=&global=1&q=" + query
+    url = "http://www.campbellized.com/"
     print(url)
-
     r = requests.get(url)
     data = r.text
+
     soup = BeautifulSoup(data, "html.parser")
+
+    img_list = soup.select("#downloads img")
+    log = ""
+
+    for img in img_list[:11]:
+        img_src = img.get("src")
+        log += img_src + "\n"
+        res = requests.get(img_src, stream=True)
+        file_name = img_src.split("/")[-1]
+
+        if res.status_code == 200:
+            print("success")
+            with open(DATA_PATH + file_name, 'wb') as file:
+                file.write(res.content)
+
+    with open(DATA_PATH + "src.txt", "w") as text_file:
+        text_file.write(log)
 
 
 def filter_input(query):
